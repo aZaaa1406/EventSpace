909ddfc3595f74e12bd03ede349d243021b1e805
aZaaa1406 <rvaldeza2200@alumno.ipn.mx>
2024-10-22 01:38:46 -0600

    backend

---
+++ backend/src/config.js
@@ -0,0 +1,2 @@
+export const PORT = 4000;
+export const URL = `http://localhost:${PORT}`
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,10 @@
 import express from 'express';
+import { PORT } from './config.js';
+import router from './routes/index.routes.js';
+const app = express();
+
+app.use(router);
+
+//server
+app.listen(PORT);
+console.log("Serven on port ", PORT);
